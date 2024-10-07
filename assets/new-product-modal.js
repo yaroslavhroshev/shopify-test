83e09d7d277b31211b17b94635a206c9929dbb28
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2024-10-08 01:08:51 +0300

    Product modal structure is completed

--- assets/new-product-modal.js
+++ assets/new-product-modal.js
@@ -2,21 +2,32 @@ class customProductModal extends HTMLElement {
   constructor() {
     super();
     this.modalContainer = this.querySelector("[data-custom-modal]");
+    this.addToCartButton = this.querySelector("[data-custom-modal-button]");
+    this.form = document.querySelector('form[data-type="add-to-cart-form"]');
   }
 
   connectedCallback() {
-    this.querySelector("[data-custom-modal-button]").addEventListener(
-      "click",
-      (e) => {
-        document.body.appendChild(this.modalContainer);
-        this.modalContainer.classList.remove("hide-modal");
-      }
-    );
+    if (this.addToCartButton) {
+      this.addToCartButton.addEventListener(
+        "click",
+        (e) => {
+          e.preventDefault();
+          // console.log(this.form)
+          // console.log(this.addToCartButton)
+          // console.log(this.addToCartButton)
+          // document.body.appendChild(this.modalContainer);
+          this.modalContainer.classList.remove("hide-modal");
+          document.body.style.overflow = "hidden";
+        }
+      );
+    }
+   
 
     this.modalContainer.addEventListener("click", (e) => {
       if (e.target.classList.contains("custom-modal__background")) {
         e.currentTarget.classList.add("hide-modal");
-        document.body.removeChild(this.modalContainer);
+        document.body.style.overflow = "auto";
+        // document.body.removeChild(this.modalContainer);
       }
     });
   }
